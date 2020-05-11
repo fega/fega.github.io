fd4fc1ca49432a50786add0c1fac07367c2ebef8
Fabian Gutierrez <fega.hg@gmail.com>
2020-05-11 16:14:38 -0500

    Updated website

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,52 +1,46 @@
 <template>
   <div>
     <HeroSection />
-    <div class="subtitle-section" id="about">
-      <sub-title>About me</sub-title>
-    </div>
-    <div class="text-section">
-      <p>I'm a FullStack developer capable to lead projects from start or working in a team for a bigger goal.</p>
-      <p>
+    <section class="v2-section" id="about">
+      <Subtitle>About</Subtitle>
+      <p
+        class="v2-text"
+      >I'm a FullStack developer capable to lead projects from start or working in a team for a bigger goal.</p>
+      <p class="v2-text">
         As a developer I feel that my mission is never stop learning and applying the industry's best practices.
         For that reason I'm also a IT security specialist and an ISO27001, quality and hard work is my spoken language.
       </p>
-      <p>
-        You can contact me at:
-      </p>
-      <br>
-      <div class="flex-section">
-        <ContactButton />
-      </div>
-    </div>
-    <div class="subtitle-section">
-      <sub-title>CodersRank Profile</sub-title>
-    </div>
-    <div class="text-section">
-      <codersrank-widget username="fega" badges="3" style=" --header-bg-color: #222"></codersrank-widget>
-    </div>
-
-    <div class="subtitle-section">
-      <sub-title>My main stack</sub-title>
-    </div>
-    <div class="flex-section">
-      <main-button v-for="tech in fabian.mainStack" :key="tech.image" color="white">
-        <img :src="tech.image" />
-        {{ tech.name }}
-      </main-button>
-    </div>
-    <div class="subtitle-section">
-      <sub-title>Experience with:</sub-title>
-    </div>
-    <div class="flex-section">
-      <main-button v-for="tech in fabian.otherStack" :key="tech.image" color="white">
-        <img :src="tech.image" />
-        {{ tech.name }}
-      </main-button>
-    </div>
-    <div class="subtitle-section" id="experience">
-      <sub-title>Node.js Professional Experience:</sub-title>
-    </div>
-    <div class="flex-section column">
+      <p class="v2-text">You can contact me at:</p>
+      <br />
+      <ContactButton />
+      <br />
+    </section>
+    <section class="v2-section" id="codersrank">
+      <Subtitle>CodersRank Profile</Subtitle>
+      <codersrank-widget username="fega" badges="3" style=" --header-bg-color: #000; --bg-color: black"></codersrank-widget>
+      <br />
+    </section>
+    <section class="v2-section" id="main-stack">
+      <Subtitle>Main Stack</Subtitle>
+      <section class="v2-section flex">
+        <main-button v-for="tech in fabian.mainStack" :key="tech.image" color="white">
+          <img :src="tech.image" />
+          {{ tech.name }}
+        </main-button>
+      </section>
+      <br />
+    </section>
+    <section class="v2-section" id="other-stack">
+      <Subtitle>Experience With:</Subtitle>
+      <section class="v2-section flex">
+        <main-button v-for="tech in fabian.otherStack" :key="tech.image" color="white">
+          <img :src="tech.image" />
+          {{ tech.name }}
+        </main-button>
+      </section>
+    </section>
+    <section class="v2-section" id="experience">
+      <Subtitle>Profesional experience:</Subtitle>
       <experience-card
         v-for="e in fabian.experience"
         :key="e.description"
@@ -57,55 +51,50 @@
         :end="e.end"
         :image="e.image"
       />
-    </div>
-    <div class="subtitle-section" id="projects">
-      <sub-title>Profesional Projects</sub-title>
-    </div>
-    <div class="text-section">
-      <p>Some of my professional work:</p>
-      <div style="display: flex; flex-wrap: wrap">
-
-      <ProjectCard 
-        v-for="project in fabian.professionalProjects" 
-        :key="project.key"
-        :title="project.title"
-        :description="project.description"
-        :image="project.image"
-        :buttons="project.buttons"/>
-      </div>
-    </div>
-    <div class="subtitle-section" id="contact">
-      <sub-title>Contact</sub-title>
-    </div>
-    <div class="flex-section">
+    </section>
+    <section class="v2-section" id="projects">
+      <Subtitle>Profesional Projects:</Subtitle>
+      <section class="v2-section flex">
+        <ProjectCard
+          v-for="project in fabian.professionalProjects"
+          :key="project.key"
+          :title="project.title"
+          :description="project.description"
+          :image="project.image"
+          :buttons="project.buttons"
+          :color="project.color"
+        />
+      </section>
+    </section>
+    <section class="v2-section" id="contact">
+      <Subtitle>Contact:</Subtitle>
       <ContactButton />
-    </div>
-    <AppFooter />
+    </section>
+    <FooterSection />
   </div>
 </template>
 
 <script>
-import HeroSection from "@/components/Hero/Fabian";
-import SubTitle from "@/components/Basic/SubTitle";
-import FlatButton from "@/components/Basic/FlatButton";
-import MainButton from "@/components/Basic/Button";
-import ExperienceCard from "@/components/Cards/Experience";
-import ProjectCard from '@/components/Cards/Project'
-import ContactButton from '@/components/Buttons/Contact';
-import AppFooter from '@/components/Basic/Footer';
+import HeroSection from "@/components/Synthwave/Hero";
+import Subtitle from "@/components/Synthwave/Subtitle";
+import ContactButton from "@/components/Synthwave/Contact";
+import MainButton from "@/components/Synthwave/Button";
+import ExperienceCard from "@/components/Synthwave/ExperienceCard";
+import ProjectCard from "@/components/Synthwave/ProjectCard";
+import FooterSection from '@/components/Synthwave/Footer';
 import { fabian } from "@/data";
 
 export default {
-  layout: "empty",
-  components: { 
-    FlatButton, 
-    MainButton, 
-    SubTitle, 
-    HeroSection, 
-    ExperienceCard, 
-    ProjectCard, 
-    ContactButton, 
-    AppFooter },
+  layout: "synth",
+  components: {
+    HeroSection,
+    ContactButton,
+    MainButton,
+    ExperienceCard,
+    FooterSection,
+    Subtitle,
+    ProjectCard
+  },
   data() {
     return {
       heroColor: {
@@ -116,7 +105,6 @@ export default {
   },
   methods: {
     setColor(ev) {
-      console.log(ev.target.attributes.color.value);
       this.heroColor.backgroundColor = ev.target.attributes.color.value;
     },
     goTo(url) {
@@ -125,6 +113,25 @@ export default {
   }
 };
 </script>
+
+<style lang="sass" scoped>
+.v2-section
+  max-width: 800px
+  margin: auto
+  margin-bottom: 20px
+  @media (max-width: 820px)
+    padding-left: 20px
+    padding-right: 20px
+  &.flex
+    display: flex
+    flex-wrap: wrap
+    margin-bottom: 0px
+    justify-content: center
+    align-items: center
+.v2-text
+  color: rgba(255,255,255,0.9)
+  margin-bottom: 10px
+</style>
 
 <style lang="scss">
 .hero {
